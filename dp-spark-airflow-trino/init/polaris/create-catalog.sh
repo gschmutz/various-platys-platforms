fecd8200a162c32aea6b7b111931e8f610d9fd18
Guido Schmutz <schmutz68@gmail.com>
2026-09-24 12:18:24 +0200

    Update create-catalog.sh

--- dp-spark-airflow-trino/init/polaris/create-catalog.sh
+++ dp-spark-airflow-trino/init/polaris/create-catalog.sh
@@ -32,7 +32,7 @@ if [ -z "$TOKEN" ]; then
 fi
 
 echo
-echo "Obtained access token: ${TOKEN}"
+echo ">> Obtained access token: ${TOKEN}"
 
 STORAGE_TYPE="FILE"
 if [ -z "${DEFAULT_STORAGE_LOCATION}" ]; then
@@ -61,7 +61,7 @@ if [ -z "${STORAGE_CONFIG_INFO}" ]; then
 fi
 
 echo
-echo Creating a catalog named $CATALOG_NAME in realm $realm...
+echo ">> Creating a catalog named $CATALOG_NAME in realm $realm..."
 
 PAYLOAD='{
    "catalog": {
@@ -85,6 +85,32 @@ curl --fail-with-body \
    -H "Polaris-Realm: $realm" \
    http://polaris:8181/api/management/v1/catalogs \
    -d "$PAYLOAD" -v
+
+echo
+echo ">> Catalog named $CATALOG_NAME created..."
+
+if [ -n "${NAMESPACE}" ]; then
+  echo
+  echo ">> Creating namespace: ${NAMESPACE}"
+
+  PAYLOAD='{
+    "namespace": ["'${NAMESPACE}'"],
+    "properties": {
+      "location": "'${NAMESPACE_STORAGE_LOCATION}/${NAMESPACE}'"
+    }
+  }'
+
+  echo $PAYLOAD
+
+  curl -s -X POST \
+    "http://polaris:8181/api/catalog/v1/${CATALOG_NAME}/namespaces" \
+    -H "Authorization: Bearer ${TOKEN}" \
+    -H "Content-Type: application/json" \
+    -d "$PAYLOAD"
+
+  echo
+  echo ">> Namespace ${NAMESPACE} created"
+fi
 
 echo
 echo Done.
